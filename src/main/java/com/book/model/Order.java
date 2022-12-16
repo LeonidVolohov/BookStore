08ce29ff87515d9f7bf01577d72150adf57986e9
Leonid Volohov <leka.volohov@gmail.com>
2022-12-16 10:23:28 +0300

    Created CRUD for Order table

--- src/main/java/com/book/model/Order.java
+++ src/main/java/com/book/model/Order.java
@@ -1,104 +1,101 @@
 package com.book.model;
 
-//import java.time.Instant;
-//import javax.persistence.*;
-//
-//@Entity
-//@Table(name = "order")
-//public class Order {
-//
-//    @Id
-//    @GeneratedValue(strategy = GenerationType.IDENTITY)
-//    @Basic(optional = false)
-//    @Column(name = "id", unique = true, nullable = false)
-//    private Long id;
-//
-//    @Column(name = "id_book", nullable = false)
-//    private Long bookId;
-//
-//    @Column(name = "id_user", nullable = false)
-//    private Long userId;
-//
-//    @Column(name = "price_per_book")
-//    private Integer pricePerBook;
-//
-//    @Column(name = "count")
-//    private Integer count;
-//
-//    @Column(name = "buy_date")
-//    private Instant buyDate;
-//
-//    @Column(name = "summary_price")
-//    private Integer summaryPrice;
-//
-//    public Order(Long bookId, Long userId, Integer pricePerBook, Integer count, Instant buyDate, Integer summaryPrice) {
-//        this.bookId = bookId;
-//        this.userId = userId;
-//        this.pricePerBook = pricePerBook;
-//        this.count = count;
-//        this.buyDate = buyDate;
-//        this.summaryPrice = summaryPrice;
-//    }
-//
-//    public Order() {}
-//
-//    public Long getId() {
-//        return id;
-//    }
-//
-//    public void setId(Long id) {
-//        this.id = id;
-//    }
-//
-//    public Long getBookId() {
-//        return bookId;
-//    }
-//
-//    public void setBookId(Long bookId) {
-//        this.bookId = bookId;
-//    }
-//
-//    public Long getUserId() {
-//        return userId;
-//    }
-//
-//    public void setUserId(Long userId) {
-//        this.userId = userId;
-//    }
-//
-//    public Integer getPricePerBook() {
-//        return pricePerBook;
-//    }
-//
-//    public void setPricePerBook(Integer pricePerBook) {
-//        this.pricePerBook = pricePerBook;
-//    }
-//
-//    public Integer getCount() {
-//        return count;
-//    }
-//
-//    public void setCount(Integer count) {
-//        this.count = count;
-//    }
-//
-//    public Instant getBuyDate() {
-//        return buyDate;
-//    }
-//
-//    public void setBuyDate(Instant buyDate) {
-//        this.buyDate = buyDate;
-//    }
-//
-//    public Integer getSummaryPrice() {
-//        return summaryPrice;
-//    }
-//
-//    public void setSummaryPrice(Integer summaryPrice) {
-//        this.summaryPrice = summaryPrice;
-//    }
-//}
+import jakarta.persistence.*;
 
+import java.time.Instant;
 
+@Entity
+@Table(name = "orders")
+public class Order {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "id", nullable = false)
+    private Integer id;
 
+//    @ManyToOne(fetch = FetchType.LAZY, optional = false)
+    @JoinColumn(name = "id_book", nullable = false)
+    private Integer idBook;
 
+//    @ManyToOne(fetch = FetchType.LAZY, optional = false)
+    @JoinColumn(name = "id_user", nullable = false)
+    private Integer idUser;
+
+    @Column(name = "price_per_book")
+    private Integer pricePerBook;
+
+    @Column(name = "count")
+    private Integer count;
+
+    @Column(name = "buy_date")
+    private Instant buyDate;
+
+    @Column(name = "summary_price")
+    private Integer summaryPrice;
+
+    public Order() {}
+
+    public Order(Integer idBook, Integer idUser, Integer pricePerBook, Integer count, Instant buyDate, Integer summaryPrice) {
+        this.idBook = idBook;
+        this.idUser = idUser;
+        this.pricePerBook = pricePerBook;
+        this.count = count;
+        this.buyDate = buyDate;
+        this.summaryPrice = summaryPrice;
+    }
+
+    public Integer getSummaryPrice() {
+        return summaryPrice;
+    }
+
+    public void setSummaryPrice(Integer summaryPrice) {
+        this.summaryPrice = summaryPrice;
+    }
+
+    public Instant getBuyDate() {
+        return buyDate;
+    }
+
+    public void setBuyDate(Instant buyDate) {
+        this.buyDate = buyDate;
+    }
+
+    public Integer getCount() {
+        return count;
+    }
+
+    public void setCount(Integer count) {
+        this.count = count;
+    }
+
+    public Integer getPricePerBook() {
+        return pricePerBook;
+    }
+
+    public void setPricePerBook(Integer pricePerBook) {
+        this.pricePerBook = pricePerBook;
+    }
+
+    public Integer getIdUser() {
+        return idUser;
+    }
+
+    public void setIdUser(Integer idUser) {
+        this.idUser = idUser;
+    }
+
+    public Integer getIdBook() {
+        return idBook;
+    }
+
+    public void setIdBook(Integer idBook) {
+        this.idBook = idBook;
+    }
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+}
